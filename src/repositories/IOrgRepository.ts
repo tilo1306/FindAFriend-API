2e0d32b35124da242806a57789e206f28eebd049
Douglas dos Santos Oliveira <doug1306@gmail.com>
2024-01-22 16:18:13 -0300

    feat: add interface findByName

--- src/repositories/IOrgRepository.ts
+++ src/repositories/IOrgRepository.ts
@@ -2,4 +2,5 @@ import { Organization, Prisma } from '@prisma/client'
 
 export interface IOrgRepository {
   create(data: Prisma.OrganizationCreateInput): Promise<Organization>
+  findByName(name: string): Promise<Organization | null>
 }
