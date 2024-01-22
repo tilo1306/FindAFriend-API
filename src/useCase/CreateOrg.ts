95ea661f2c481aa7733a0f52d7abb0580d7422d2
Douglas dos Santos Oliveira <doug1306@gmail.com>
2024-01-22 15:51:33 -0300

    feat: add interface de request and response

--- src/useCase/CreateOrg.ts
+++ src/useCase/CreateOrg.ts
@@ -2,16 +2,33 @@ import { IOrgRepository } from '@/repositories/IOrgRepository'
 import { Organization } from '@prisma/client'
 import { hash } from 'bcryptjs'
 
+interface ICreateOrgUseCaseRequest {
+  name: string
+  email: string
+  password: string
+  zip_code: string
+  address: string
+  state: string
+  city: string
+  phone: string
+}
+
+interface ICreateOrgUseCaseResponse {
+  org: Organization
+}
+
 export class CreateOrgUseCase {
   constructor(private orgsRepository: IOrgRepository) {}
 
-  async execute(data: Organization): Promise<Organization> {
+  async execute(
+    data: ICreateOrgUseCaseRequest,
+  ): Promise<ICreateOrgUseCaseResponse> {
     const passwordHash = await hash(data.password, 6)
 
     data.password = passwordHash
 
     const org = await this.orgsRepository.create(data)
 
-    return org
+    return { org }
   }
 }
