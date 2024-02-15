bce2566c313b6c3991228651f718ca264747a8b4
Douglas dos Santos Oliveira <doug1306@gmail.com>
2024-02-15 18:07:09 -0300

    feat: add route register and ero zod

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,15 @@ import swagger from '@fastify/swagger'
 import swaggerUi from '@fastify/swagger-ui'
 import { SwaggerTheme } from 'swagger-themes'
 import { SwaggerThemeNameEnum } from 'swagger-themes/build/enums/swagger-theme-name'
+import { orgsRoutes } from './http/controllers/orgs/routes'
+import { env } from './env'
+import { ZodError } from 'zod'
 
 export const app = fastify({ logger: true })
 
 app.register(swagger)
+app.register(orgsRoutes)
+
 app.register(swaggerUi, {
   routePrefix: 'docs',
   theme: {
@@ -17,4 +22,20 @@ app.register(swaggerUi, {
       },
     ],
   },
+})
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: 'Validation error.', issues: error.format() })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  } else {
+    // TODO: Here we should log to a external tool like DataDog/NewRelic/Sentry
+  }
+
+  return reply.status(500).send({ message: 'Internal server error.' })
 })
