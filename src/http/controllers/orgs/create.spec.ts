ddae9fd8ca5a2501e8076a8b4bdce55837801dff
Douglas dos Santos Oliveira <doug1306@gmail.com>
2024-02-16 13:32:39 -0300

    test: e2e register orgs

---
+++ src/http/controllers/orgs/create.spec.ts
@@ -0,0 +1,91 @@
+import { app } from '@/app'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+describe('Register (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('Should be able to register', async () => {
+    const res = await request(app.server).post('/orgs').send({
+      name: 'Test org pets',
+      email: 'test@test.com',
+      password: '123456789',
+      zip_code: '0123456',
+      address: 'Rua do Teste n010101',
+      state: 'Testelandia',
+      city: 'teste',
+      phone: '11998877445566',
+    })
+
+    expect(res.statusCode).toEqual(201)
+  })
+
+  it('Should not be able to register name org aready exits', async () => {
+    const body = {
+      name: 'Test org pets',
+      email: 'test@test.com',
+      password: '123456789',
+      zip_code: '0123456',
+      address: 'Rua do Teste n010101',
+      state: 'Testelandia',
+      city: 'teste',
+      phone: '11998877445566',
+    }
+
+    await request(app.server).post('/orgs').send(body)
+
+    const res = await request(app.server).post('/orgs').send({
+      name: 'Test org pets',
+      email: 'node@node.com',
+      password: '123456789',
+      zip_code: '0123456',
+      address: 'Rua do Teste n010101',
+      state: 'Testelandia',
+      city: 'teste',
+      phone: '11998877445566',
+    })
+    console.log(res)
+
+    expect(res.statusCode).toEqual(409)
+    expect(res.body.message).toEqual(
+      'Name Organization already or email exists.',
+    )
+  })
+
+  it('Should not be able to register email aready exits', async () => {
+    const body = {
+      name: 'Test org pets',
+      email: 'test@test.com',
+      password: '123456789',
+      zip_code: '0123456',
+      address: 'Rua do Teste n010101',
+      state: 'Testelandia',
+      city: 'teste',
+      phone: '11998877445566',
+    }
+
+    await request(app.server).post('/orgs').send(body)
+
+    const res = await request(app.server).post('/orgs').send({
+      name: 'Test org pets1',
+      email: 'test@test.com',
+      password: '123456789',
+      zip_code: '0123456',
+      address: 'Rua do Teste n010101',
+      state: 'Testelandia',
+      city: 'teste',
+      phone: '11998877445566',
+    })
+    console.log(res)
+
+    expect(res.statusCode).toEqual(409)
+    expect(res.body.message).toEqual(
+      'Name Organization already or email exists.',
+    )
+  })
+})
