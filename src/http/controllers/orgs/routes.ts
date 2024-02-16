ed95f4a22b14d344fc14d0da14410390ef0bdf93
Douglas dos Santos Oliveira <doug1306@gmail.com>
2024-02-16 18:07:52 -0300

    chore: add swagger route

--- src/http/controllers/orgs/routes.ts
+++ src/http/controllers/orgs/routes.ts
@@ -2,5 +2,41 @@ import { FastifyInstance } from 'fastify'
 import { create } from './create'
 
 export async function orgsRoutes(app: FastifyInstance) {
-  app.post('/orgs', create)
+  app.post(
+    '/orgs',
+    {
+      schema: {
+        summary: 'Create a Orgs',
+        description: 'Create a new Orgs',
+        tags: ['Orgs'],
+        body: {
+          type: 'object',
+          properties: {
+            name: { type: 'string' },
+            email: { type: 'string' },
+            password: { type: 'string' },
+            zip_code: { type: 'string' },
+            address: { type: 'string' },
+            state: { type: 'string' },
+            city: { type: 'string' },
+            phone: { type: 'string' },
+          },
+        },
+        response: {
+          201: {
+            description: 'Succesful response',
+            type: 'object',
+          },
+          409: {
+            description: 'Organization name or email already exists.',
+            type: 'object',
+            properties: {
+              message: { type: 'string' },
+            },
+          },
+        },
+      },
+    },
+    create,
+  )
 }
