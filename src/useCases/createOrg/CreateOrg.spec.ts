05d2c199d75d692040813efcfd51cc5f4e9de1f9
Douglas dos Santos Oliveira <doug1306@gmail.com>
2024-02-17 12:54:26 -0300

    test: Ajustrole return test

--- src/useCases/createOrg/CreateOrg.spec.ts
+++ src/useCases/createOrg/CreateOrg.spec.ts
@@ -34,7 +34,7 @@ describe('Create ORG Use Case', () => {
     expect(org.state).toEqual(data.state)
     expect(org.city).toEqual(data.city)
     expect(org.phone).toEqual(data.phone)
-    expect(org.role).toEqual('ADMIN')
+    expect(org.role).toEqual('ORG')
   })
 
   it('Should not be able to register ORG name already exists', async () => {
