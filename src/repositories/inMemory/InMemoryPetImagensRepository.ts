bbdefd1b69275fd2c80b2c638d3706d796ffa65e
Douglas dos Santos Oliveira <doug1306@gmail.com>
2024-02-17 22:23:51 -0300

    feat: create InMemoryPetImagens

---
+++ src/repositories/inMemory/InMemoryPetImagensRepository.ts
@@ -0,0 +1,19 @@
+import { ICreatePetImagensDTO } from '@/dtos/ICreatePetImagensDTO'
+import { IPetImagensRepository } from '../IPetImagensRepository'
+import { PetImages } from '@prisma/client'
+import { randomUUID } from 'node:crypto'
+
+export class InMemoryPetImagensRepository implements IPetImagensRepository {
+  public db: PetImages[] = []
+  async create({ petId, urlImage }: ICreatePetImagensDTO): Promise<PetImages> {
+    const petImagens: PetImages = {
+      id: randomUUID(),
+      url: urlImage,
+      pet_id: petId,
+    }
+
+    this.db.push(petImagens)
+
+    return petImagens
+  }
+}
