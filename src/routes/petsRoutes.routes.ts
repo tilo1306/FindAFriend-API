28b52eb42ddd7368c501465f75f70c8a5eae6a87
Douglas dos Santos Oliveira <doug1306@gmail.com>
2024-02-18 22:17:01 -0300

    feat: routepet

---
+++ src/routes/petsRoutes.routes.ts
@@ -0,0 +1,40 @@
+import { create } from '@/http/controllers/pets/create'
+import { verifyJWT } from '@/http/middlewares/verifyJwt'
+import { FastifyInstance } from 'fastify'
+
+export async function petsRoute(app: FastifyInstance) {
+  app.addHook('onRequest', verifyJWT)
+  app.post(
+    '/pets',
+    {
+      schema: {
+        summary: 'Create a pet',
+        description: 'Create a new pet',
+        tags: ['Pets'],
+        body: {
+          type: 'object',
+          properties: {
+            name: { type: 'string' },
+            description: { type: 'string' },
+            old: { type: 'string' },
+            energyLevel: { type: 'string' },
+            independenceLevel: { type: 'string' },
+            environment: { type: 'string' },
+            gender: { type: 'string' },
+            state: { type: 'string' },
+            city: { type: 'string' },
+            requiredNeeds: { type: 'array', items: { type: 'string' } },
+            petImage: { type: 'string' },
+          },
+        },
+        response: {
+          201: {
+            description: 'Succesful response',
+            type: 'object',
+          },
+        },
+      },
+    },
+    create,
+  )
+}
