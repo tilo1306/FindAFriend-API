c03bbf02924dc3ba2e8fb3be8b419e0898c921f3
Douglas dos Santos Oliveira <doug1306@gmail.com>
2024-02-20 20:46:09 -0300

    feat: create findpet method

--- src/repositories/IPetsRepository.ts
+++ src/repositories/IPetsRepository.ts
@@ -1,6 +1,8 @@
 import { ICreatePetDTO } from '@/dtos/ICreatePetDTO'
+import { IFilterPetsDTO } from '@/dtos/IFilterPetsDTO'
 import { Pet } from '@prisma/client'
 
 export interface IPetsRepository {
   create(data: ICreatePetDTO): Promise<Pet>
+  petsFilter(data: IFilterPetsDTO): Promise<Pet[]>
 }
