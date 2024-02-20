26f4579411186e7a2f752f9e67f6eaef1d1df77b
Douglas dos Santos Oliveira <doug1306@gmail.com>
2024-02-20 20:46:27 -0300

    feat: create findpetimagem method

--- src/repositories/IPetImagensRepository.ts
+++ src/repositories/IPetImagensRepository.ts
@@ -3,4 +3,5 @@ import { PetImages } from '@prisma/client'
 
 export interface IPetImagensRepository {
   create(data: ICreatePetImagensDTO): Promise<PetImages>
+  findPetImage(petId: string): Promise<PetImages[]>
 }
