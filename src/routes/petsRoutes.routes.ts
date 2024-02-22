bb8cb3dfe20e155d3e498acfcd5637fe351c0198
Douglas dos Santos Oliveira <doug1306@gmail.com>
2024-02-22 16:59:23 -0300

    feat: Add route filterPet and schema fastify

--- src/routes/petsRoutes.routes.ts
+++ src/routes/petsRoutes.routes.ts
@@ -1,4 +1,5 @@
 import { create } from '@/http/controllers/pets/create'
+import { filterPet } from '@/http/controllers/pets/filterPet'
 import { verifyJWT } from '@/http/middlewares/verifyJwt'
 import { FastifyInstance } from 'fastify'
 
@@ -28,6 +29,7 @@ export async function petsRoute(app: FastifyInstance) {
             gender: { type: 'string' },
             state: { type: 'string' },
             city: { type: 'string' },
+            size: { type: 'string' },
             requiredNeeds: { type: 'array', items: { type: 'string' } },
             petImage: { type: 'string' },
           },
@@ -38,7 +40,7 @@ export async function petsRoute(app: FastifyInstance) {
             type: 'object',
           },
           401: {
-            description: 'Succesful response',
+            description: 'Unauthorized response',
             type: 'object',
             properties: {
               message: { type: 'string' },
@@ -48,5 +50,115 @@ export async function petsRoute(app: FastifyInstance) {
       },
     },
     create,
+  )
+  app.get(
+    '/pets/search',
+    {
+      schema: {
+        summary: 'Filter pets ',
+        description: 'Filter pets',
+        tags: ['Pets'],
+        security: [
+          {
+            bearerAuth: [],
+          },
+        ],
+        querystring: {
+          type: 'object',
+          required: ['city'],
+          properties: {
+            city: {
+              type: 'string',
+              description: 'Value to filter by city',
+            },
+            page: {
+              description: 'Value page for pagination',
+              type: 'number',
+            },
+            old: {
+              description: 'Value old pet',
+              type: 'string',
+              enum: ['PUPPY', 'ADULT', 'SENIOR'],
+            },
+            size: {
+              description: 'Value size pet',
+              type: 'string',
+              enum: ['SMALL', 'AVERAGE', 'BIG'],
+            },
+            energyLevel: {
+              description: 'Value energy level pet',
+              type: 'string',
+              enum: ['LOW', 'AVERAGE', 'HIGH'],
+            },
+            independenceLevel: {
+              description: 'Value independence level pet',
+              type: 'string',
+              enum: ['LOW', 'AVERAGE', 'HIGH'],
+            },
+            environment: {
+              description: 'Value environment pet',
+              type: 'string',
+              enum: ['OPEN', 'environment'],
+            },
+            gender: {
+              description: 'Value gender pet',
+              type: 'string',
+              enum: ['Femea', 'Macho'],
+            },
+            state: {
+              description: 'Value state org',
+              type: 'string',
+              enum: ['Femea', 'Macho'],
+            },
+            requiredNeeds: {
+              description: 'Value required needs Pet',
+              type: 'string',
+            },
+          },
+        },
+        response: {
+          200: {
+            description: 'Succesful response',
+            type: 'object',
+            properties: {
+              data: {
+                type: 'array',
+                items: {
+                  type: 'object',
+                  properties: {
+                    id: { type: 'string' },
+                    name: { type: 'string' },
+                    description: { type: 'string' },
+                    energy_level: { type: 'string' },
+                    independence_level: { type: 'string' },
+                    environment: { type: 'string' },
+                    gender: { type: 'string' },
+                    state: { type: 'string' },
+                    city: { type: 'string' },
+                    requiredNeeds: { type: 'string' },
+                    created_at: { type: 'string' },
+                    ong_id: { type: 'string' },
+                    imagens: {
+                      type: 'array',
+                    },
+                  },
+                },
+              },
+              page: {
+                type: 'number',
+              },
+            },
+          },
+          401: {
+            description: 'Unauthorized response',
+            type: 'object',
+            properties: {
+              message: { type: 'string' },
+            },
+          },
+        },
+      },
+    },
+    filterPet,
   )
 }
